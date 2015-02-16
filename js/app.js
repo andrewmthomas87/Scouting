2c1d67292b637e4da4a4f5f9debfcd5debb6db51
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-16 16:12:58 -0700

    Added stacking functionality

--- js/app.js
+++ js/app.js
@@ -49,57 +49,89 @@ $(document).ready(function() {
 			currentView = 2;
 		}
 	});
-	$('div#palette>div').on('dragstart', paletteDivDrag);
-	addSpacerEvents($('div.spacer'));
-});
 
-function paletteDivDrag(event) {
-	var objects = '';
-	$(this).find('div').each(function() {
-		objects += $(this).attr('class') + ',';
+	// PALETTE DIV EVENTS
+
+	$('div#palette>div').on('dragstart', function(event) {
+		var objects = '';
+		$(this).find('div').each(function() {
+			objects += $(this).attr('class') + ',';
+		});
+		event.originalEvent.dataTransfer.setData('objects', objects);
 	});
-	event.originalEvent.dataTransfer.setData('type', objects);
-}
-
-function droppableEnter(event) {
-	event.originalEvent.preventDefault();
-	$(this).addClass('active');
-}
-
-function droppableOver(event) {
-	event.originalEvent.preventDefault();
-}
-
-function droppableLeave(event) {
-	event.originalEvent.preventDefault();
-	$(this).removeClass('active');
-}
-
-function spacerStart(event) {
-	event.originalEvent.preventDefault;
-}
-
-function spacerDrop(event) {
-	event.originalEvent.preventDefault();
-	var objects = event.originalEvent.dataTransfer.getData('type').split(',');
-	var stack = $('<div></div>');
-	for (i = 0; i < objects.length; i++) {
-		stack.append('<div class="' + objects[i] + '"></div>');
-	}
-	var stackContainer = $('<div class="stack"></div>');
-	stackContainer.append(stack);
-	var spacer = $('<div class="spacer"></div>');
-	addSpacerEvents(spacer);
-	$(this).after(spacer);
-	$(this).after(stackContainer);
-	$(this).removeClass('active');
-}
 
 
-function addSpacerEvents(spacer) {
-	spacer.on('dragstart', spacerStart);
-	spacer.on('dragenter', droppableEnter);
-	spacer.on('dragover', droppableOver);
-	spacer.on('dragleave', droppableLeave);
-	spacer.on('drop', spacerDrop);
-}
+	// SPACER EVENTS
+
+	$('div#main section').delegate('div.spacer', 'dragenter', function(event) {
+		event.originalEvent.preventDefault();
+		$(this).addClass('active');
+	});
+	$('div#main section').delegate('div.spacer', 'dragover', function(event) {
+		event.originalEvent.preventDefault();
+	});
+	$('div#main section').delegate('div.spacer', 'dragleave', function(event) {
+		event.originalEvent.preventDefault();
+		$(this).removeClass('active');
+	});
+	$('div#main section').delegate('div.spacer', 'drop', function(event) {
+		event.stopPropagation();
+		event.originalEvent.preventDefault();
+		var objects = event.originalEvent.dataTransfer.getData('objects').split(',');
+		var stack = $('<div></div>');
+		for (i = 0; i < objects.length - 1; i++) {
+			stack.append('<div class="' + objects[i] + '"></div>');
+		}
+		var stackContainer = $('<div class="stack" draggable="true"></div>');
+		stackContainer.append(stack);
+		var spacer = $('<div class="spacer"></div>');
+		$(this).after(spacer);
+		$(this).after(stackContainer);
+		$('div.selection').remove();
+		$(this).removeClass('active');
+	});
+
+	// STACK EVENTS
+
+	$('div#main section').delegate('div.stack', 'dragstart', function(event) {
+		var objects = '';
+		$(this).find('div div').each(function() {
+			objects += $(this).attr('class') + ',';
+		});
+		event.originalEvent.dataTransfer.setData('objects', objects);
+		event.originalEvent.dataTransfer.setDragImage($(this).find('>div')[0], $(this).width() / 2, $(this).find('>div').height() / 2);
+		$(this).addClass('selection');
+		$(this).next('div.spacer').addClass('selection');
+		$('div.selection').hide(125);
+	});
+	$('div#main section').delegate('div.stack', 'dragend', function(event) {
+		console.log(event);
+		if (event.originalEvent.dataTransfer.dropEffect == 'none') {
+			$('div.selection').show(125);
+			$('div.selection').removeClass('selection');
+		}
+	});
+	$('div#main section').delegate('div.stack', 'dragenter', function(event) {
+		event.originalEvent.preventDefault();
+		$(this).addClass('active');
+	});
+	$('div#main section').delegate('div.stack', 'dragover', function(event) {
+		event.originalEvent.preventDefault();
+	});
+	$('div#main section').delegate('div.stack', 'dragleave', function(event) {
+		event.originalEvent.preventDefault();
+		$(this).removeClass('active');
+	});
+	$('div#main section').delegate('div.stack', 'drop', function(event) {
+		event.stopPropagation();
+		event.originalEvent.preventDefault();
+		var objects = event.originalEvent.dataTransfer.getData('objects').split(',');
+		var stack = $(this).find('>div');
+		for (i = 0; i < objects.length - 1; i++) {
+			stack.prepend('<div class="' + objects[objects.length - 2 - i] + '"></div>');
+		}
+		$('div.selection').remove();
+		$(this).removeClass('active');
+	});
+
+});
