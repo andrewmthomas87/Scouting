450feb179c3aa88757ea23ce56fad07ee2f2bbbb
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-21 17:00:04 -0700

    Added logic to stacking and trash

--- js/app.js
+++ js/app.js
@@ -75,19 +75,22 @@ $(document).ready(function() {
 		$(this).removeClass('active');
 	});
 	$('div#main section').delegate('div.spacer', 'drop', function(event) {
-		event.stopPropagation();
 		event.originalEvent.preventDefault();
-		var objects = event.originalEvent.dataTransfer.getData('objects').split(',');
-		var stack = $('<div></div>');
-		for (i = 0; i < objects.length - 1; i++) {
-			stack.append('<div class="' + objects[i] + '"></div>');
+		var objects = event.originalEvent.dataTransfer.getData('objects');
+		if (objects.indexOf('litter,') < 0 && objects.indexOf('scoring-platform') < 0) {
+			event.stopPropagation();
+			objects = objects.split(',');
+			var stack = $('<div></div>');
+			for (i = 0; i < objects.length - 1; i++) {
+				stack.append('<div class="' + objects[i] + '"></div>');
+			}
+			var stackContainer = $('<div class="stack" draggable="true"></div>');
+			stackContainer.append(stack);
+			var spacer = $('<div class="spacer"></div>');
+			$(this).after(spacer);
+			$(this).after(stackContainer);
+			$('div.selection').remove();
 		}
-		var stackContainer = $('<div class="stack" draggable="true"></div>');
-		stackContainer.append(stack);
-		var spacer = $('<div class="spacer"></div>');
-		$(this).after(spacer);
-		$(this).after(stackContainer);
-		$('div.selection').remove();
 		$(this).removeClass('active');
 	});
 
@@ -103,13 +106,15 @@ $(document).ready(function() {
 		$(this).addClass('selection');
 		$(this).next('div.spacer').addClass('selection');
 		$('div.selection').hide(125);
+		$('a#trash').fadeIn(125);
 	});
 	$('div#main section').delegate('div.stack', 'dragend', function(event) {
-		console.log(event);
-		if (event.originalEvent.dataTransfer.dropEffect == 'none') {
+		if (event.originalEvent.dataTransfer.dropEffect == 'none' || !event.isPropagationStopped()) {
 			$('div.selection').show(125);
 			$('div.selection').removeClass('selection');
 		}
+		$('a#trash').removeClass('active');
+		$('a#trash').fadeOut(125);
 	});
 	$('div#main section').delegate('div.stack', 'dragenter', function(event) {
 		event.originalEvent.preventDefault();
@@ -123,15 +128,77 @@ $(document).ready(function() {
 		$(this).removeClass('active');
 	});
 	$('div#main section').delegate('div.stack', 'drop', function(event) {
+		event.originalEvent.preventDefault();
+		var objects = event.originalEvent.dataTransfer.getData('objects');
+		var invalid = false;
+		var hasBin = $(this).find('div div.bin').length > 0;
+		var alreadyBinned = objects.indexOf('bin') > -1 && hasBin;
+		var alreadyLittered = objects.indexOf('litter') > -1 && $(this).find('div div.litter, div div.litter-binned').length > 0;
+		var alreadyScored = objects.indexOf('scoring-platform') > -1 && $(this).find('div div.scoring-platform').length > 0;
+		var invalidYellowTotePlacement = objects.indexOf('yellow-tote') > -1 && ($(this).find('div div.yellow-tote').length < 1 || $(this).find('div div.yellow-tote').length > 2);
+		var invalidPlacementUponYellowToteStack = objects.indexOf('yellow-tote') < 0 && $(this).find('div div.yellow-tote').length > 0;
+		invalid = alreadyBinned || alreadyLittered || alreadyScored || invalidYellowTotePlacement || invalidPlacementUponYellowToteStack;
+		if (objects.indexOf('litter,') > -1) {
+			if ($(this).find('div div.bin').length > 0) {
+				objects = objects.replace('litter', 'litter-binned');
+			}
+			else {
+				invalid = true;
+			}
+		}
+		objects = objects.split(',');
+		var totes = 0;
+		for (i = 0; i < objects.length - 1; i++) {
+			totes += objects[i].indexOf('tote') > -1 ? 1 : 0;
+		}
+		invalid = invalid || totes + $(this).find('div div.tote, div div.chute-tote').length > 6;
+		if (!invalid) {
+			var stack = $(this).find('>div');
+			var object;
+			for (i = 0; i < objects.length - 1; i++) {
+				object = '<div class="' + objects[objects.length - 2 - i] + '"></div>';
+				if (object.indexOf('scoring-platform') > -1) {
+					stack.append(object);
+				}
+				else if (!hasBin || object.indexOf('litter') > -1) {
+					stack.prepend(object);
+				}
+				else if (stack.find('div').length > 1) {
+					stack.find('div').not('.scoring-platform').last().after('<div class="' + objects[i] + '"></div>');
+				}
+				else {
+					stack.append(object);
+				}
+			}
+			$('div.selection').remove();
+			event.stopPropagation();
+		}
+		$(this).removeClass('active');
+	});
+
+
+	// Trash events
+
+	$('a#trash').on('dragenter', function(event) {
+		event.originalEvent.preventDefault();
+		$(this).addClass('active');
+	});
+
+	$('a#trash').on('dragover', function(event) {
+		event.originalEvent.preventDefault();
+	});
+
+	$('a#trash').on('dragleave', function(event) {
+		event.originalEvent.preventDefault();
+		$(this).removeClass('active');
+	});
+
+	$('a#trash').on('drop', function(event) {
 		event.stopPropagation();
 		event.originalEvent.preventDefault();
-		var objects = event.originalEvent.dataTransfer.getData('objects').split(',');
-		var stack = $(this).find('>div');
-		for (i = 0; i < objects.length - 1; i++) {
-			stack.prepend('<div class="' + objects[objects.length - 2 - i] + '"></div>');
-		}
 		$('div.selection').remove();
 		$(this).removeClass('active');
+		$(this).fadeOut(125);
 	});
 
 });
