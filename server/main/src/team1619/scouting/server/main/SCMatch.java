53b88d76cd050576965cb7b7259d55b8a11074e7
Terry Olkin <terry.olkin@gridcraft.com>
2015-03-04 22:17:28 -0700

    Reworked match assignments.

--- server/main/src/team1619/scouting/server/main/SCMatch.java
+++ server/main/src/team1619/scouting/server/main/SCMatch.java
@@ -1,35 +1,99 @@
 package team1619.scouting.server.main;
 
+import team1619.scouting.server.utils.SCLogger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by avimoskoff on 3/3/15.
  */
-public class SCMatch {
+public class SCMatch
+{
+    public static class MatchTeamData
+    {
+        private final int fTeamNumber;
+        private final String fAlliance;
+
+        public MatchTeamData( int teamNumber, String alliance )
+        {
+            fTeamNumber = teamNumber;
+            fAlliance = alliance;
+        }
+
+        public int getTeamNumber()
+        {
+            return fTeamNumber;
+        }
+
+        public String getAlliance()
+        {
+            return fAlliance;
+        }
+    }
+
     private static SCMatch sCurrentMatch;
-    int fMatchNumber;
-    int[] fTeams;
-    int fNextTeamIndex;
 
-    public static void newMatch(int matchNumber, int redTeam1, int redTeam2, int redTeam3, int blueTeam1, int blueTeam2, int blueTeam3) {
-        sCurrentMatch = new SCMatch(matchNumber, redTeam1, redTeam2, redTeam3, blueTeam1, blueTeam2, blueTeam3);
+    private int fMatchNumber;
+
+    private Map<Integer, MatchTeamData> fTeamsMap;
+
+    private List<MatchTeamData> fTeams;
+
+    public static void newMatch( int matchNumber, int redTeam1, int redTeam2, int redTeam3, int blueTeam1, int blueTeam2, int blueTeam3 )
+    {
+        sCurrentMatch = new SCMatch( matchNumber, redTeam1, redTeam2, redTeam3, blueTeam1, blueTeam2, blueTeam3 );
     }
 
-    private SCMatch(int matchNumber, int redTeam1, int redTeam2, int redTeam3, int blueTeam1, int blueTeam2, int blueTeam3) {
+    private SCMatch( int matchNumber, int redTeam1, int redTeam2, int redTeam3, int blueTeam1, int blueTeam2, int blueTeam3 )
+    {
         fMatchNumber = matchNumber;
-        fTeams = new int[6];
-        fTeams[0] = redTeam1;
-        fTeams[1] = redTeam2;
-        fTeams[2] = redTeam3;
-        fTeams[3] = blueTeam1;
-        fTeams[4] = blueTeam2;
-        fTeams[5] = blueTeam3;
-        fNextTeamIndex = 0;
+
+        fTeams = new ArrayList<>( 6 );
+
+        fTeams.add( new MatchTeamData( redTeam1, "red" ) );
+        fTeams.add( new MatchTeamData( redTeam2, "red" ) );
+        fTeams.add( new MatchTeamData( redTeam3, "red" ) );
+        fTeams.add( new MatchTeamData( blueTeam1, "blue" ) );
+        fTeams.add( new MatchTeamData( blueTeam2, "blue" ) );
+        fTeams.add( new MatchTeamData( blueTeam3, "blue" ) );
+
+        // initialize empty client to team map
+        fTeamsMap = new HashMap<>();
     }
 
-    public static int getNextTeam() {
-        return sCurrentMatch.fTeams[sCurrentMatch.fNextTeamIndex++];
+    public static MatchTeamData getNextTeam( Integer clientId )
+    {
+        return sCurrentMatch.lookupTeamData( clientId );
     }
 
-    public static int getMatchNumber() {
+    private MatchTeamData lookupTeamData( Integer clientId )
+    {
+        MatchTeamData teamData = fTeamsMap.get( clientId );
+
+        if ( teamData == null )
+        {
+            if ( fTeams.isEmpty() )
+            {
+                SCLogger.getLogger().error( "Client trying to get a team when all were assigned" );
+                return null;
+            }
+
+            // assign this client to the next team data
+            teamData = fTeams.remove( 0 );
+
+            SCLogger.getLogger().info( "Assigning team %d to scout %d", teamData.getTeamNumber(), clientId );
+
+            fTeamsMap.put( clientId, teamData );
+        }
+
+        return teamData;
+    }
+
+    public static int getMatchNumber()
+    {
         return sCurrentMatch.fMatchNumber;
     }
 }
--- server/main/src/team1619/scouting/server/main/SCPrepareNextMatch.java
+++ server/main/src/team1619/scouting/server/main/SCPrepareNextMatch.java
@@ -8,20 +8,35 @@ import java.sql.SQLException;
 /**
  * Created by avimoskoff on 2/19/15.
  */
-public class SCPrepareNextMatch extends SCMessage {
+public class SCPrepareNextMatch extends SCMessage
+{
 
-    public SCPrepareNextMatch() {
-
+    public SCPrepareNextMatch()
+    {
     }
 
     @Override
-    void processMessage(MySQL conn, SCJSON message) throws SQLException
+    void processMessage( MySQL conn, SCJSON message ) throws SQLException
     {
         SCJSON outboundMessage = new SCJSON();
+
         SCClientQueue clientQueue = SCOutbound.getClientQueue( getClientID() );
-        outboundMessage.put( "matchNumber", SCMatch.getMatchNumber() );
-        outboundMessage.put( "teamNumber", SCMatch.getNextTeam() );
-        outboundMessage.put( "alliance", "red" );  // TODO: correct color
+
+        SCMatch.MatchTeamData matchData = SCMatch.getNextTeam( getClientID() );
+
+        if ( matchData == null )
+        {
+            outboundMessage.put( "type", "status" );
+            outboundMessage.put( "status", "no-match-available" );
+        }
+        else
+        {
+            outboundMessage.put( "type", "assignedTeam" );
+            outboundMessage.put( "matchNumber", SCMatch.getMatchNumber() );
+            outboundMessage.put( "teamNumber", matchData.getTeamNumber() );
+            outboundMessage.put( "alliance", matchData.getAlliance() );
+        }
+
         clientQueue.writeToClient( outboundMessage );
     }
 }
